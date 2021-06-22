d8ce7c3d7e7dacdf2acb465b4e6b80592c6a9189
rovallec <raul.ovalle@nearsol.gt>
2021-06-22 14:50:04 -0600

    search button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Articles } from './articles';
 import { AuthServiceService } from './auth-service.service';
 import { Users } from './users';
 
@@ -17,9 +18,14 @@ export class AppComponent {
   current_site:string = 'Global';
   global_dir:string = 'Inactive';
   selectedOption:string = 'HOME';
+  searchText:string = null;
+  search:boolean = false;
+  selected_article:Articles = new Articles;
 
   users: Users[] = [];
   sites: string[] = [];
+  allArticles:Articles[] = [];
+  searchRes:Articles[] = [new Articles];
 
   ngOnInit() {
     this.users = [{
@@ -39,6 +45,83 @@ export class AppComponent {
     }]
 
     this.sites = ['Global', 'Guatemala', 'Manila', 'Iloilo', 'Colombia'];
+
+    this.allArticles = [
+      {
+        location:"Global",
+        header:"Core Value of the Month",
+        byline: "Results-Driven",
+        multimedia:"http://181.114.12.81/intranet/assets/homepage_image.png",
+        fragment: "Being a person that is results-driven means that you are focused and obsessed with achieving a great outcome.",
+        article:"Being a person that is results-driven means that you are focused and obsessed with achieving a great outcome. You don’t want to just compete; you want to WIN!",
+        author:'HR',
+        date:'June 17, 2021',
+        url: 'https://nearsol.us/',
+        origin:"Manila"
+      },
+      {
+        location:"Global",
+        header:"Pride Month 2021",
+        byline: "",
+        multimedia:"http://181.114.12.81/intranet/assets/hr_image_5.png",
+        fragment: "June is PRIDE Month! It's time to celebrate our courageous and colorful brothers and sisters in the LGBTQIA+ community! Be proud of who you are and say #IAMINCLUDED",
+        article:"June is PRIDE Month! It's time to celebrate our courageous and colorful brothers and sisters in the LGBTQIA+ community! Be proud of who you are and say #IAMINCLUDED",
+        author:'HR',
+        date:'June 17, 2021',
+        url: '',
+        origin:"Manila"
+      },
+      {
+        location:"Global",
+        header:"Father's Day at NEARSOL",
+        byline: '',
+        multimedia:"http://181.114.12.81/intranet/assets/hr_image_1.png",
+        fragment: "It's time to honor and recognize our hardworking and awesome Fathers/Dads/Papas/Tatays/Papsy at NEARSOL.",
+        article:"It's time to honor and recognize our hardworking and awesome Fathers/Dads/Papas/Tatays/Papsy at NEARSOL. Show your love and appreciation to all Dads by creating a TikTok video for a chance to win awesome prizes during the Father’s Day Virtual Party on June 18, 2021! #NearsolSuperDad.",
+        author:'HR',
+        date:'June 17, 2021',
+        url: '',
+        origin:"Manila"
+      },
+      {
+        location:'Global',
+        header:"NIA",
+        byline: 'NEARSOL Intelligence Automation',
+        multimedia:"http://181.114.12.81/intranet/assets/recruitment_image_1.png",
+        fragment: "We proudly present to you NIA, NEARSOL Recruitment Chatbot Recruitment Tool. NIA helps us with lead generation, process guidance and does all that while projecting our brand identity.",
+        article:"We proudly present to you NIA, NEARSOL Recruitment Chatbot Recruitment Tool. NIA helps us with lead generation, process guidance and does all that while projecting our brand identity. She is not just a chatbot, she is so much more!",
+        author:'HR',
+        date:"June 17, 2021",
+        url: 'https://nearsol.us/nia-manila/',
+        origin:"Manila"
+      },
+      {
+        location:'Global',
+        header:"Guatemala",
+        byline: '',
+        multimedia:"./assets/gt_image_1.jpg",
+        fragment: "",
+        article:"",
+        author:'HR',
+        date:"June 17, 2021",
+        url: '',
+        origin:"Guatemala"
+      },
+      {
+        location:'Global',
+        header:"Colombia",
+        byline: '',
+        multimedia:"./assets/cl_image_1.png",
+        fragment: "",
+        article:"",
+        author:'HR',
+        date:"June 17, 2021",
+        url: '',
+        origin:"Colombia"
+      }
+    ];
+
+
   }
 
   getAuth(): boolean {
@@ -74,5 +157,26 @@ export class AppComponent {
 
   SetSel(sel:string){
     this.selectedOption = sel;
+  }
+
+  setSearch(){
+    this.search = !this.search;
+  }
+
+  searchNow(){
+    this.searchRes = [];
+    this.allArticles.forEach(art=>{
+      if(art.article.toUpperCase().includes(this.searchText.toUpperCase()) || art.header.toUpperCase().includes(this.searchText.toUpperCase()) || art.byline.toUpperCase().includes(this.searchText.toUpperCase())){
+        this.searchRes.push(art);
+      }
+    })
+  }
+
+  keyupSearch(){
+    document.getElementById("btn_search").click();
+  }
+
+  setSelection(selected:Articles){
+    this.selected_article = selected;
   }
 }
