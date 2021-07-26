fc3e8300ae275047ae7fc18264339092a0f4a7f2
avielman <avielman@gmail.com>
2021-07-25 22:10:35 -0600

    corrección menor, se corrige la ruta del php, para desarrollo y producción.

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Users } from './users';
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class ApiServiceService {
 
 
   //PHP_API_SERVER = "http://172.18.2.45";
-  PHP_API_SERVER = "http://181.114.12.81";
+  PHP_API_SERVER = environment.PHP_URL;
 
   constructor(private httpClient:HttpClient) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  PHP_URL: 'http://181.114.12.81'
 };
