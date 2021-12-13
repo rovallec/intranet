9a1103a4a1fe5c3558330d76c366a0b6e0d99e99
rovallec <raul.ovalle@nearsol.gt>
2021-12-13 10:21:52 -0600

    test

--- src/app/approval-request/approval-request.component.ts
+++ src/app/approval-request/approval-request.component.ts
@@ -613,10 +613,10 @@ export class ApprovalRequestComponent implements OnInit {
   insertAdjustment() {
     this.apiService.insertAttJustification(this.attAdjudjment).subscribe((str: string) => {
         this.apiService.sendNotification({id:this.workingEmployee.idemployees, date:this.todayDate, type:'Attendance Justification',
-        status:this.attAdjudjment.status, description:"<tr><td style='color:white;background-color:#FF4237'>RELATED DATE:</td><td>" + this.attAdjudjment.attendance_date + "</td>/tr>" + 
-        "<tr><td style='color:white;background-color:#FF4237'>RASON:</td><td>" + this.attAdjudjment.reason + "</td>/tr>" + 
-        "<tr><td style='color:white;background-color:#FF4237'>TIME BEFORE:</td><td>" + this.attAdjudjment.time_before + "</td>/tr>" +
-        "<tr><td style='color:white;background-color:#FF4237'>RELATED DATE:</td><td>" + this.attAdjudjment.time_after + "</td>/tr>"}).subscribe((str2:string)=>{
+        status:this.attAdjudjment.status, description:"<tr><td style='color:white;background-color:#003B71'>RELATED DATE:</td><td>" + this.attAdjudjment.attendance_date + "</td></tr>" + 
+        "<tr><td style='color:white;background-color:#003B71'>RASON:</td><td>" + this.attAdjudjment.reason + "</td></tr>" + 
+        "<tr><td style='color:white;background-color:#003B71'>TIME BEFORE:</td><td>" + this.attAdjudjment.time_before + "</td></tr>" +
+        "<tr><td style='color:white;background-color:#003B71'>RELATED DATE:</td><td>" + this.attAdjudjment.time_after + "</td></tr>"}).subscribe((str2:string)=>{
           if(str == '1'){
             window.alert("Request placed successfully");
           }
